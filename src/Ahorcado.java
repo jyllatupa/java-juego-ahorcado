a0dd3aa1b64b3e7c38342986c7b5957becb965e6
jyllatupa <chelsea_30_01@hotmail.com>
2024-09-26 19:52:59 -0500

    primer commit del juego ahorcado en JAVA

---
+++ src/Ahorcado.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Ahorcado {
+    public static void main(String[] args) throws Exception {
+        
+        // Clase scanner que nos permite que el usuario escriba
+        Scanner scanner = new Scanner(System.in);
+
+        // Declaraciones y asignaciones
+        String palabraSecreta = "inteligencia";
+        int intentosMaximos = 10;
+        int intentos = 0;
+        boolean palabraAdivinada = false;
+
+        //Arreglos
+        char[] letrasAdivinadas = new char[palabraSecreta.length()];
+
+        // bucle
+        for (int i = 0; i < letrasAdivinadas.length; i++) {
+            letrasAdivinadas[i] = '_';
+        }
+
+        // blucle while
+        while(!palabraAdivinada && intentos < intentosMaximos){
+            System.out.println("Palabra a adivinar: " +  String.valueOf(letrasAdivinadas) + " (" + palabraSecreta.length() + "letras)");
+            System.out.println("Introduce una letra, por favor");
+
+            // Usamos la clase scanner para pedir una letra
+            char letra = Character.toLowerCase(scanner.next().charAt(0));
+
+            boolean letraCorrecta = false;
+
+            for (int i = 0; i < palabraSecreta.length(); i++) {
+                if(palabraSecreta.charAt(i) == letra){
+                    letrasAdivinadas[i] = letra;
+                    letraCorrecta = true;
+                }
+            }
+
+            if(!letraCorrecta){
+                intentos++;
+                System.out.println("Incorrecto! Te quedan " + (intentosMaximos - intentos) + " intentos.");
+            }
+
+            if(String.valueOf(letrasAdivinadas).equals(palabraSecreta)){
+                palabraAdivinada = true;
+                System.out.println("Felicidades, has adivinado la palabra secreta: " + palabraSecreta);
+            }
+        }
+
+        if(!palabraAdivinada){
+            System.out.println("Que pena, te has quedado sin intentos! GAME OVER");
+        }
+
+        scanner.close();
+    }
+}
